343495798ac385e914470cdd259f0e08ced1685f
Aarron Kerin <twiggy1988@googlemail.com>
2017-10-06 14:46:06 +0100

    added stuff

--- SupportBank/SupportBank/Account.cs
+++ SupportBank/SupportBank/Account.cs
@@ -7,8 +7,32 @@ using System.Threading.Tasks;
 namespace SupportBank
 {
     class Accounts
+
     {
         public string Name { get; set; }
-        public (List<Transaction> Transactions, string name)
+        public List<Transaction> Transactions { get; set; }
+
+        public Decimal GetBalance()
+        {
+            Decimal balance = 0;
+            foreach (var transaction in Transactions)
+            {
+                if (transaction.From == Name)
+                {
+                    balance = balance - transaction.Amount;
+                }
+                else if (transaction.To == Name)
+                {
+                    balance = balance + transaction.Amount;
+                }
+            }
+            return balance;
+
+
+
+        }
+
     }
-}
+}  
+ 
+
--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -13,9 +13,9 @@ namespace SupportBank
         {
 
            
-
+            
             var reader = new StreamReader(File.OpenRead(@"C:\Work\Training\SupportBank\Transactions2014.csv"));
-
+            Dictionary<string, Accounts> accounts = new Dictionary<string, Accounts>();
             List<Transaction> Transactions = new List<Transaction>();
 
             
@@ -23,7 +23,7 @@ namespace SupportBank
             Console.WriteLine("1) List accounts and balances");
             Console.WriteLine("2) List all transactions on accounts");
 
-            if (  "1")
+            
 
 
             reader.ReadLine();
@@ -34,11 +34,65 @@ namespace SupportBank
                 var splitline = line.Split(',');
 
                 Transaction Eachtransaction = new Transaction(DateTime.Parse(splitline[0]), splitline[1], splitline[2], splitline[3], Decimal.Parse(splitline[4]));
+                Transactions.Add(Eachtransaction);
 
 
 
                 Console.WriteLine(splitline[0] + " " + splitline[1] + " " + splitline[2] + " " + splitline[3] + " " + splitline[4]);
                 
+            }
+
+            foreach (var transaction in Transactions)
+            {
+                // accounts dictionary has the key "transaction.From"
+                if (accounts.ContainsKey(transaction.From))
+                {
+                    accounts[transaction.From].Transactions.Add(transaction);
+                }
+                else
+                {
+                    List<Transaction> accounttrans = new List<Transaction>();
+                    accounttrans.Add(transaction);
+
+                    Accounts account = new Accounts();
+                    account.Name = transaction.From;
+                    account.Transactions = accounttrans;
+
+
+                    string name = transaction.From;
+
+                    accounts.Add(name, account);
+
+
+
+                    // Add new Account to dictionary with key: account.Name, value: account 
+
+                }
+                if (accounts.ContainsKey(transaction.To))
+
+                {
+                    accounts[transaction.To].Transactions.Add(transaction);
+                }
+                else
+                { 
+                    List<Transaction> accounttrans = new List<Transaction>();
+                    accounttrans.Add(transaction);
+
+                    Accounts account = new Accounts();
+                    account.Name = transaction.To;
+                    account.Transactions = accounttrans;
+
+
+                    string name = transaction.To;
+
+                    accounts.Add(name, account);
+                }
+
+            }
+
+            foreach (var account in accounts.Values)
+            {
+                Console.WriteLine(account.Name + ": £" + account.GetBalance());
             }
             
             Console.ReadLine();
